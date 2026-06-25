c6e1ac17d5187b90999cc359d317f2c29a5091bb
saahil004 <saahilghulammohammad@gmail.com>
2026-06-25 12:15:21 +0500

    remove .env from tracking

---
+++ Backend/app/core/security.py
@@ -0,0 +1,22 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 # 24 hours expiry
+
+def create_access_token(data : dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp" : expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithms=[ALGORITHM])
+
+def verify_token(token : str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError: # if token invalid or expired
+        return None
+    
+# add SECRET_KEY to .env    
